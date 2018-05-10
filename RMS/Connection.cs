a119a7fa9bb02afcd2e50e77c237236bf4fbfb9a
Hamza Raza <hmzatech@hotmail.com>
2018-05-10 12:49:36 +0500

    Database Connectivity

---
+++ RMS/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RMS
+{
+    class Connection
+    {
+        string conStr = System.Configuration.ConfigurationManager.ConnectionStrings["DB"].ToString();
+        public string Connect()
+        {
+            return conStr;
+        }
+    }
+}
